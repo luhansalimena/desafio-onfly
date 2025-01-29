4aa68307ad88f5b913d1debf2817239f7a057749
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-29 00:00:15 -0300

    Add route for storing trip orders with authentication

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TripOrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-
+Route::middleware('auth:api')->group(function () {
+    Route::post('/trip-orders', [TripOrderController::class, 'store'])->name('trip-orders.store');
+});
