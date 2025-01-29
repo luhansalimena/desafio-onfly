e4ffa400b19ddc850232da082c4156b71e68e9cf
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-28 23:55:19 -0300

    Add TripOrder model with fillable attributes

---
+++ app/Models/TripOrder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TripOrder extends Model
+{
+    /** @use HasFactory<\Database\Factories\TripOrderFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'requester_name',
+        'status',
+        'from',
+        'to',
+        'trip_date',
+        'trip_return_date',
+        'user_id'
+    ];
+}
