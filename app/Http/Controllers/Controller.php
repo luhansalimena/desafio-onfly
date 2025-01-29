998b39aac6e329f4fd05fd2cdbec3df5321ef638
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-28 22:54:39 -0300

    Add OpenAPI documentation annotations to Controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,25 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="Desafio OnFly API",
+ *     version="1.0.0",
+ *     description="API Documentation for Desafio OnFly application",
+ *     @OA\Contact(
+ *         email="lsalimena6@gmail.com",
+ *         name="Luhan Salimena"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * ),
+ * @OA\Server(
+ *     url="http://localhost/api",
+ *     description="Local API Server"
+ * )
+ */
 abstract class Controller
 {
     //
