906310b1bd06d5b6fb9aa12a6557b83ad6a5b50b
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-29 22:11:13 -0300

    Add feature tests for searching trip orders with various parameters

---
+++ tests/Feature/SeachTripOrdersTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TripOrder;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class SeachTripOrdersTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_success_when_searching_trips_with_no_parameters(): void
+    {
+        $user = User::factory()->create();
+
+        TripOrder::factory(10)->create();
+        $validTrip = TripOrder::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user,'api')->get(route('trip-orders.index'));
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'current_page',
+            'data' => [
+                '*' => [
+                    'id',
+                    'status',
+                    'from',
+                    'to',
+                    'trip_date',
+                    'trip_return_date'
+                ]
+            ]
+        ]);
+
+        $response->assertJsonFragment([
+            'id' => $validTrip->id,
+            'status' => str($validTrip->status)->upper(),
+            'from' => $validTrip->from,
+            'to' => $validTrip->to,
+            'trip_date' => $validTrip->trip_date,
+            'trip_return_date' => $validTrip->trip_return_date,
+        ]);
+    }
+
+    public function test_success_when_searching_trips_with_status_parameter(): void
+    {
+        $user = User::factory()->create();
+
+        TripOrder::factory(10)->create();
+        $validTrip = TripOrder::factory()->create(['user_id' => $user->id, 'status' => 'requested']);
+
+        $response = $this->actingAs($user, 'api')->get(route('trip-orders.index', ['status' => 'requested']));
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'current_page',
+            'data' => [
+                '*' => [
+                    'id',
+                    'status',
+                    'from',
+                    'to',
+                    'trip_date',
+                    'trip_return_date'
+                ]
+            ]
+        ]);
+
+        $response->assertJsonFragment([
+            'id' => $validTrip->id,
+            'status' => str($validTrip->status)->upper(),
+            'from' => $validTrip->from,
+            'to' => $validTrip->to,
+            'trip_date' => $validTrip->trip_date,
+            'trip_return_date' => $validTrip->trip_return_date,
+        ]);
+    }
+
+    public function test_failure_when_searching_trips_with_invalid_parameter(): void
+    {
+        $user = User::factory()->create();
+
+        TripOrder::factory(10)->create();
+
+        $response = $this->actingAs($user, 'api')->get(route('trip-orders.index', ['status' => 'invalid_status']));
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'status'
+            ]
+        ]);
+    }
+
+
+}
