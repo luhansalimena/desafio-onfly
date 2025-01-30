b8abf9dcf9b8125c80fc8e95faca2bd1ef8737e3
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-30 13:33:33 -0300

    Refactor trip order status validation to separate method and add past trip cancellation check

--- app/Actions/TripOrders/UpdateTripOrders.php
+++ app/Actions/TripOrders/UpdateTripOrders.php
@@ -6,6 +6,7 @@ use App\Models\TripOrder;
 use App\Notifications\TripOrderStatusChanged;
 use App\OrderStatus;
 use App\Repositories\Interface\TripOrderRepositoryInterface;
+use Carbon\Carbon;
 use Illuminate\Validation\ValidationException;
 
 
@@ -19,14 +20,23 @@ class UpdateTripOrders
             throw ValidationException::withMessages(['Only requested orders can be approved or cancelled']);
         }
 
-        if(!in_array($data['status'], [OrderStatus::APPROVED->value, OrderStatus::CANCELLED->value])) {
-            throw ValidationException::withMessages(['Cannot update order status to ' . $data['status']]);
-        }
+        $this->validateStatus($tripOrder, $data['status']);
 
         $tripOrder = $this->tripOrderRepository->update($tripOrder, $data);
 
         $tripOrder->user->notify(new TripOrderStatusChanged($data['status']));
 
         return $tripOrder;
+    }
+
+    public function validateStatus(TripOrder $tripOrder, string $status): void
+    {
+        if(!in_array($status, [OrderStatus::APPROVED->value, OrderStatus::CANCELLED->value])) {
+            throw ValidationException::withMessages(['Cannot update order status to ' . $status]);
+        }
+
+        if($status === OrderStatus::CANCELLED->value && Carbon::createFromDate($tripOrder->trip_date)->isPast()) {
+            throw ValidationException::withMessages(['Cannot cancel order for past trips']);
+        }
     }
 }
