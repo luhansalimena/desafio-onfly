cf53483b0ba3eb37292e28c2427fca80c056c67d
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-29 21:50:35 -0300

    Add search method to TripOrderRepository for filtering and pagination

--- app/Repositories/Interface/TripOrderRepositoryInterface.php
+++ app/Repositories/Interface/TripOrderRepositoryInterface.php
@@ -3,8 +3,11 @@
 namespace App\Repositories\Interface;
 
 use App\Models\TripOrder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 interface TripOrderRepositoryInterface
 {
     public function create(array $data): TripOrder;
+    public function search(array $filters, bool $paginated = true): Collection | LengthAwarePaginator;
 }
--- app/Repositories/TripOrderRepository.php
+++ app/Repositories/TripOrderRepository.php
@@ -2,13 +2,37 @@
 
 namespace App\Repositories;
 
+use App\Filters\TripOrderFilter;
 use App\Models\TripOrder;
 use App\Repositories\Interface\TripOrderRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 
 class TripOrderRepository implements TripOrderRepositoryInterface
 {
-    public function create(array $data): TripOrder{
+
+    public function __construct(
+        private TripOrderFilter $filter
+    ) {}
+
+    public function create(array $data): TripOrder
+    {
         return TripOrder::create($data);
+    }
+
+    public function search(array $filters, bool $paginated = true): Collection | LengthAwarePaginator
+    {
+        $query = TripOrder::query();
+
+        if (!empty($filters)) {
+            $query = $this->filter->apply($query, $filters);
+        }
+
+        if ($paginated) {
+            return $query->paginate();
+        }
+
+        return $query->get();
     }
 }
