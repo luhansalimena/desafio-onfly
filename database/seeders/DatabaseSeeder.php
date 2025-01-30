5b31b3456d8c9e92666e7b4c5b95ea7afc435fba
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-30 16:39:47 -0300

    Seed database with additional user and trip orders for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         TripOrder::factory(10)->create();
-        TripOrder::factory(10)->create(
-            ['user_id' => 1]
-        );
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
+
+        TripOrder::factory(10)->create(
+            ['user_id' => 11]
+        );
     }
 }
