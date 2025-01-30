fc560cd77efbe5d366b0695359d828e23ea6f9a0
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-29 23:21:59 -0300

    Add UpdateOrdersRequest for validating trip order status updates

---
+++ app/Http/Requests/UpdateOrdersRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\OrderStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateOrdersRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if(auth()->user()->id == $this->tripOrder->user_id) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => ['required', 'string', Rule::in(array_column(OrderStatus::cases(), 'value'))],
+        ];
+    }
+
+    public function prepareForValidation(): void
+    {
+        $this->merge([
+            'status' => (string) str($this->status)->upper(),
+        ]);
+    }
+}
